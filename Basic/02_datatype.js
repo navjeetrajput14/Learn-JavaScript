18e900a982c3e9dc96c9255b41dc0a0ff104eac8
Navjeet Rajput <96377013+navjeetrajput14@users.noreply.github.com>
2024-09-22 10:23:37 +0000

    Define and Use

---
+++ Basic/02_datatype.js
@@ -0,0 +1,19 @@
+// "use strict"; //treat all JS code as newer version
+
+// alert(3+3)  //we are using nodejs , not browser 
+
+console.log(3+3);
+
+
+// number =2 to power 53
+//bigint
+//string=> ""
+// boolean => true / false
+// null=> standalone value .( type is object)
+//undefined=>value is not define
+// symbol=>finding the uniqueness
+
+
+
+// object
+console.log(typeof null);
