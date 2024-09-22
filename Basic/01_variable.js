cb4f7f6cdf849cf405b8b491492942c04fb92c44
Navjeet Rajput <96377013+navjeetrajput14@users.noreply.github.com>
2024-09-22 09:55:24 +0000

    Define and Use

---
+++ Basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=13112 //can not change
+let accountEmail="navjeetrajput1@gmail.com"
+var accountPassword="12313"
+accountCity="Jaipur"
+let accountstate;
+
+// accountId=2 //not allowed
+console.log(accountId);
+
+accountEmail="nr@gmail.com"
+accountPassword="3242"
+accountCity="Nagina"
+/*
+prefer not to use Var
+Because of issue in block scope and function scope
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountstate]);
