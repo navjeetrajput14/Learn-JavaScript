bd437fd6a00d243ca155bceca21c41a80d2c090d
Navjeet Rajput <96377013+navjeetrajput14@users.noreply.github.com>
2024-09-25 09:17:07 +0000

    Summary

--- 01_basic/dataType_Summary.js
+++ 01_basic/dataType_Summary.js
@@ -1,3 +1,20 @@
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+
+
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
@@ -35,3 +52,30 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// *********************************************************
+
+
+// Stack->primitive (copy of variable)
+// heap=>Non-Primitive(reference of variable [change in the orginal variable])
+
+let name1="navjeet"
+let newName=name1
+newName="Manan"
+console.log(name1);
+console.log(newName);
+
+let userOne= {
+     Email:"abc@gmail.com",
+     upiId:"asbb@ybl"
+
+}
+let usertwo=userOne
+usertwo.Email="aslan@gmail.com"
+console.log(userOne.Email);
+console.log(usertwo.Email);
+
+
+
+
