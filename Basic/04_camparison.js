5fb75dfe859286515517e963451115b079734982
Navjeet Rajput <96377013+navjeetrajput14@users.noreply.github.com>
2024-09-25 08:47:40 +0000

    Camparison

---
+++ Basic/04_camparison.js
@@ -0,0 +1,12 @@
+console.log(2>1);
+console.log(2<=1);
+console.log(2>=1);
+
+
+
+console.log("2"===2); // difftrent data type
+console.log(2===2);
+
+console.log("dkabkbkbcskbjcasn");
+
+console.log("2">=2)
