be1539533af95083a0007322f87112ca04cbcdae
Navjeet Rajput <96377013+navjeetrajput14@users.noreply.github.com>
2024-10-08 07:52:45 +0000

    arrow

---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,11 @@
+const user={
+    username:"Navjeet",
+    price:9,
+    WelcomeMessage: function(){
+        console.log(`${this.username} Welcome to my website`);
+        
+    }
+    
+}
+user.WelcomeMessage()
+console.log(10);
